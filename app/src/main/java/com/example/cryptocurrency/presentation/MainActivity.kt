9174221192465c8c98a6ddd75cb8d2d07ae94047
HEETAE HEO <97378151+heetaeheo@users.noreply.github.com>
2022-07-10 12:21:44 +0900

    Hilt 및 Compile error

---
+++ app/src/main/java/com/example/cryptocurrency/presentation/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.cryptocurrency.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.cryptocurrency.presentation.coin_detail.CoinDetailScreen
+import com.example.cryptocurrency.presentation.coin_list.CoinListScreen
+import com.example.cryptocurrency.presentation.ui.CryptocurrencyAppYTTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CryptocurrencyAppYTTheme {
+                Surface(color = MaterialTheme.colors.background) {
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.CoinListScreen.route
+                    ) {
+                        composable(
+                            route = Screen.CoinListScreen.route
+                        ) {
+                            CoinListScreen(navController)
+                        }
+                        composable(
+                            route = Screen.CoinDetailScreen.route + "/{coinId}"
+                        ) {
+                            CoinDetailScreen()
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
